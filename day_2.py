1a2ba3d9bccc75b79d295a63f8c7b58d7e5cc590
Marjan-maxa <boktimd95@gmail.com>
2026-08-10 01:05:34 +0600

    day -2 successfully completed

---
+++ day_2.py
@@ -0,0 +1,110 @@
+name="Marjan"
+age=23
+isStudent=True
+print("Name :",type(name))
+print("Age :",type(age))
+print(type(isStudent))
+
+number1=12
+number2=6
+result=number1+number2
+print(result)
+
+a="24"
+b=int(a)
+print(type(a))
+print(type(b))
+
+name=input("Enter your Name: ")
+age=input("Enter your Age :")
+print("My name is :",name)
+print("My Age : ",age)
+print("Age type :",type(age))
+
+
+
+
+
+# challange 1
+a=int(input("Enter your  first number :"))
+b=int(input("Enter your  first number :"))
+print("Sum is :",a+b)
+print("Sub is :",a-b)
+print("Multy is :",a*b)
+print("Dvision is :",a/b)
+
+
+#challenge 2
+
+name="Marjan"
+age=23
+cgpa=3.75
+is_student=True
+print(name,type(name))
+print(age,type(age))
+print(cgpa,type(cgpa))
+print(is_student,type(is_student))
+
+
+
+
+#chalenge 3
+
+number="100"
+price="99.50"
+c=int(number)
+d=float(price)
+print(type(c))
+print(type(d))
+
+
+#challenge 4
+
+Bangla=80
+English=75
+Math=90
+total=Bangla+English+Math
+print("Total Mark :",total)
+print("Average Mark :",(total/3))
+
+
+#challenge 5
+
+Name=input("Enter your Name :")
+Age=input("Enter your Age :")
+Department=input("Enter your Department :")
+print("===== Student Information =====")
+print("Name :",Name)
+print("Age :",Age)
+print("Deartment :",Department)
+
+
+
+#mini project 1(Simple Calculator)
+
+print("===== SIMPLE CALCULATOR =====")
+
+a=int(input("Enter First Number a:"))
+
+b=int(input("Enter Second Number b:"))
+
+print("Sum is     :",a+b)
+print("Sub is     :",a-b)
+print("Multy is   :",a*b)
+print("Dvision is :",a/b)
+print("======================================")
+
+
+
+#mini project 2(Student Result Calculator)
+print("===== STUDENT RESULT =====")
+Name=input("Enter your Name           :")
+B=int(input("Enter your Bangla Marks  :"))
+E=int(input("Enter your English Marks :"))
+M=int(input("Enter your Math Marks    :"))
+total=B+E+M
+avg=float(total/3)
+
+print("Total Marks              :",total)
+print("Average                  :",avg)
+print("==========================")
